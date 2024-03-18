0b2e797ed4aa9361c98a78d57fab27999dc2d413
Diego Costa ⚜ <124840180+DiegoCstyles@users.noreply.github.com>
2024-03-18 14:11:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -379,11 +379,6 @@ async function createIdUser(customFieldValues) {
     }
   }
 
-app.get('/oauth/callback', (req, res) => {
-    const authorizationCode = req.query.code;
-    res.send('Código de autorização recebido: ' + authorizationCode);
-});
-
 // Set the server to listen on port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
