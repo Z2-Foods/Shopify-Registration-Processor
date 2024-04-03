4407b5870fc866091045a7ffd6d05e8ac74cc836
Diego Costa ⚜ <124840180+DiegoCstyles@users.noreply.github.com>
2024-04-03 16:15:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ async function createCompany(customFieldValues, provinceCode) {
           "input": {
             "company": {
               "externalId": "",
-              "name": customFieldValues.FirstName || "Default Name"
+              "name": customFieldValues.FirstName
             },
             "companyLocation": {
               "billingAddress": {
